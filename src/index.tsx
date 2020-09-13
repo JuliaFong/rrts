3b39c7e385f405f55ad334a4fc80ce844bc5e8e1
Julia Fong <Juliamzfong@gmail.com>
2020-09-13 13:29:52 -0500

    Add component state

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+interface AppProps {
+    color?: string;
+}
+
+
 class App extends React.Component {
+   state = { counter: 0 }
+
+    onIncrement = ():void => {
+        this.setState({ counter: this.state.counter + 1})
+    }
+
+    onDecrement = ():void => {
+        this.setState({ counter: this.state.counter - 1})
+    }
+
     render() {
-        return <div>Hi there</div>
+        return <div>
+            <button onClick={this.onIncrement}>Increment</button>
+            <button onClick={this.onDecrement}>Decrement</button>
+            {this.state.counter}
+        </div>
     }
 }
 
 ReactDOM.render(
-<App />, 
+<App  />, 
 document.querySelector('#root')
 )
