dbc2b7613e184b3b9578fbb8a61fd4ad50c6c675
Julia Fong <Juliamzfong@gmail.com>
2020-09-13 15:00:25 -0500

    Add reducer and app folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App } from './components/App';
+import { reducers } from './reducer'
 
-interface AppProps {
-    color?: string;
-}
 
-class App extends React.Component {
-   state = { counter: 0 }
-
-    onIncrement = ():void => {
-        this.setState({ counter: this.state.counter + 1})
-    }
-
-    onDecrement = ():void => {
-        this.setState({ counter: this.state.counter - 1})
-    }
-
-    render() {
-        return <div>
-            <button onClick={this.onIncrement}>Increment</button>
-            <button onClick={this.onDecrement}>Decrement</button>
-            {this.state.counter}
-        </div>
-    }
-}
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-<App  />, 
+    <Provider store={store}>
+        <App  />
+    </Provider>,
 document.querySelector('#root')
 )
