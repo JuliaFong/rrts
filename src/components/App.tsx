1aed61e71e78c2c8bd4c645abd944e6e9f632511
Julia Fong <Juliamzfong@gmail.com>
2020-09-13 20:19:51 -0500

    Debug app.tsx file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,8 +5,8 @@ import { StoreState } from '../reducers';
 
 interface AppProps {
     todos: Todo[];
-    fetchTodos(): any;
-    deleteTodo(): any;
+    fetchTodos: typeof fetchTodos;
+    deleteTodo: typeof deleteTodo;
 }
 
 export class _App extends React.Component<AppProps> {
